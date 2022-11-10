a969853c5794ddfc8938a107ae24e1a4220bde0f
phil5520 <phil5520@CV-BB-7592>
2022-11-10 09:15:33 +0100

    Made Services non-nullable, and added first HttpGet request

--- Services/SOSUService.cs
+++ Services/SOSUService.cs
@@ -1,7 +1,23 @@
-﻿namespace Services
+﻿using Services.Interfaces;
+using Newtonsoft.Json;
+
+namespace Services
 {
-    public class SOSUService
+    public class SOSUService<TEntity> : ISOSUService<TEntity> where TEntity : class
     {
-        
+        protected static string baseUrl = "";
+
+        public async Task<List<TEntity>> DoHttpGetRequest(string ControllerUrl)
+        {
+            var returnResponse = new List<TEntity>();
+            using (var client = new HttpClient())
+            {
+                string url = $"{baseUrl}/{ControllerUrl}";
+                var apiResponse = await client.GetAsync(url);
+
+                returnResponse = JsonConvert.DeserializeObject<List<TEntity>>(await apiResponse.Content.ReadAsStringAsync());
+                return returnResponse;
+            }
+        }
     }
 }
