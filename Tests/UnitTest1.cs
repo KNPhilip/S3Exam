a8a5fa388ca5e2022ab056f841b9a62d774971e8
phil5520 <phil5520@CV-BB-7592>
2022-11-10 14:58:23 +0100

    Final touches on my not-finished-but-need-to-be-kinda-done-exam :(

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,11 +1,65 @@
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Tests
 {
-    public class UnitTest1
+    public class GenericRepositoryMethodsTests
     {
         [Fact]
-        public void Test1()
+        public void AssignmentRepositoryGetByIdMethodReturnsCorrectOutput()
         {
+            //Arrange
+            SOSUPowerContext context = new();
+            UnitOfWork unitOfWork = new(context);
+            GenericRepository<Assignment> a = unitOfWork.AssignmentRepository;
+            int AssignmentId = 1;
+            int expectedOutputAssignment = AssignmentId;
+            int actualOutputAssignment;
 
+            //Act
+            Assignment assignment = a.GetByID(AssignmentId);
+            actualOutputAssignment = assignment.Id;
+
+            //Assert
+            Assert.Equal(expectedOutputAssignment, actualOutputAssignment);
+        }
+
+        [Fact]
+        public void ResidentRepositoryGetByIdMethodReturnsCorrectOutput()
+        {
+            //Arrange
+            SOSUPowerContext context = new();
+            UnitOfWork unitOfWork = new(context);
+            GenericRepository<Resident> r = unitOfWork.ResidentRepository;
+            int ResidentId = 2;
+            int expectedOutputResident = ResidentId;
+            int actualOutputResident;
+
+            //Act
+            Resident resident = r.GetByID(ResidentId);
+            actualOutputResident = resident.Id;
+
+            //Assert
+            Assert.Equal(expectedOutputResident, actualOutputResident);
+        }
+
+        [Fact]
+        public void EmployeeRepositoryGetByIdMethodReturnsCorrectOutput()
+        {
+            //Arrange
+            SOSUPowerContext context = new();
+            UnitOfWork unitOfWork = new(context);
+            GenericRepository<Employee> e = unitOfWork.EmployeeRepository;
+            int EmployeeId = 2;
+            int expectedOutputEmployee = EmployeeId;
+            int actualOutputEmployee;
+
+            //Act
+            Employee employee = e.GetByID(EmployeeId);
+            actualOutputEmployee = employee.Id;
+
+            //Assert
+            Assert.Equal(expectedOutputEmployee, actualOutputEmployee);
         }
     }
 }
